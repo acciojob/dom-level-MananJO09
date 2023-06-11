b75a0fdccfff8b6ca9c79def32045c70cf8d38a5
MananJO09 <87749897+MananJO09@users.noreply.github.com>
2023-06-11 09:23:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your JS code here. If required.
+ var listItem = document.getElementById("level");
+  var level = 0;
+
+  while (listItem.parentElement) {
+    level++;
+    listItem = listItem.parentElement;
+  }
+
+  alert("The level of the element is: " + level);
